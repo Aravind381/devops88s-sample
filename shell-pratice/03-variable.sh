eab6f38b59489f13178794ab13e107d0a6bc2cc4
Your Name <aravindsivalasetty.381@gmail.com>
2026-01-29 18:41:36 -0600

    changed something

---
+++ shell-pratice/03-variable.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+person1=suresh
+person2=ramesh
+
+echo "bo$person1 :: hello $person2, how are you?"
+echo "$person2:: i am fine"
+echo "$person1:: good $person2"
+echo "$person2:: ok fine"
